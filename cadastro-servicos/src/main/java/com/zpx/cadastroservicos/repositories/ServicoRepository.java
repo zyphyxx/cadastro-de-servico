73d7a1a8c93d7b3d6c2f3666bd953308f0eeee48
Edgar Jesus <mendesbr92@gmail.com>
2024-02-10 16:18:16 -0300

    feat: criado uma query personalizada para os metodos findPayments

--- cadastro-servicos/src/main/java/com/zpx/cadastroservicos/repositories/ServicoRepository.java
+++ cadastro-servicos/src/main/java/com/zpx/cadastroservicos/repositories/ServicoRepository.java
@@ -2,6 +2,17 @@ package com.zpx.cadastroservicos.repositories;
 
 import com.zpx.cadastroservicos.entities.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ServicoRepository extends JpaRepository <Servico, Long> {
+
+    @Query("select s from Servico s where s.valorPago <> null and s.valorPago > 0 ")
+    List<Servico> findAllPaymentsPendents();
+
+    @Query("select s from Servico s where s.status = 'cancelado' ")
+    List<Servico> findAllPaymentsCanceleds();
+
+
 }
